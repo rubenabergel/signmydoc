85a6a83b6e4938e10e272f2c518b3c34cac5ce94
Ruben Abergel <ruben.abergel@gmail.com>
2015-05-07 11:36:52 -0700

    typo

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.get('/callback', function(req, res){
 
 
 app.post('/callback', function(req, res){
-  console.log('request', req.params);
-  console.log('response', res);
+  console.log('REQUEST AND RESPONSE');
+
   res.status(200).send('Hello API Event Received');
 });
 
