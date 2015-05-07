e9ee4dcd2687cc9f3b07e416f2b194c74bc7c50a
Ruben Abergel <ruben.abergel@gmail.com>
2015-05-07 10:14:40 -0700

    response

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.get('/callback', function(req, res){
 
 
 app.post('/callback', function(req, res){
-    console.log('req', req);
-    console.log('res', res);
+  res.status(200).send('Hello API Event Received');
 
-    res.end('some response data');
+
+    // res.end('some response data');
 
 });
 
